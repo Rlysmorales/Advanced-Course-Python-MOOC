bbc55e06f541b3f68d4cda6b59b52f380de41cce
Arelys Morales <rlysmorales@gmail.com>
2023-10-03 22:40:50 -0400

    Finished Lesson 8

--- Lesson_8/11_first_and_last_name/src/first_and_last_name.py
+++ Lesson_8/11_first_and_last_name/src/first_and_last_name.py
@@ -1,7 +1,30 @@
 # Write your solution here:
+class Person:
 
+    def __init__(self, name: str):
+        self.name = name
 
+    def return_first_name(self):
+        first_name = self.name.split()
+        
+        return first_name[0]
+        
+    def return_last_name(self):
+        first_name = self.name.split()
+        
+        return first_name[1]
 
+def main():
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
+
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
+
+if __name__ == "__main__":
+    main()
 
 
 
--- Lesson_8/12_number_stats/src/number_stats.py
+++ Lesson_8/12_number_stats/src/number_stats.py
@@ -2,9 +2,49 @@
 class  NumberStats:
     def __init__(self):
         self.numbers = 0
-
+        self.count = 0
+    
     def add_number(self, number:int):
-        pass
-
+           self.count += 1
+           self.numbers += number
+    
     def count_numbers(self):
-        pass
+        return self.count
+    
+    def get_sum(self):
+         if self.numbers == 0:
+              return 0
+         else:
+              return self.numbers
+         
+    def average(self):
+         try:
+            return self.numbers / self.count
+         except ZeroDivisionError:
+             ...
+    
+def user_input():
+    all_numbers = NumberStats()
+    even_numbers = NumberStats()
+    odd_numbers = NumberStats()
+
+    while True: 
+        user_input = int(input("Please type in integer numbers:"))
+        if user_input % 2 == 0:
+            even_numbers.add_number(user_input)   
+        elif user_input < 0:
+            print("Sum of numbers:", all_numbers.get_sum())
+            print("Mean of numbers:", all_numbers.average())
+            print("Sum of even numbers", even_numbers.get_sum())
+            print("Sum of odd numbers", odd_numbers.get_sum())
+            break
+        elif user_input % 2 == 1:
+            odd_numbers.add_number(user_input)
+
+        if user_input >= 0: 
+            all_numbers.add_number(user_input)
+
+user_input()
+
+
+
